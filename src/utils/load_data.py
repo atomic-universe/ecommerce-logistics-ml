1624c2b7db13f373f82e95c509c7a7b2aab492d2
Vighnesh <vighneshsankpal7@gmail.com>
2026-03-01 20:08:16 +0530

    update data loading script and handled errors

--- src/utils/load_data.py
+++ src/utils/load_data.py
@@ -1,18 +1,50 @@
 from connect_mysql import Connect_MySQL
+import os
 
+# import mysql connectivity credentials 
+from credentials  import database_connection_credentials as connectivity_info
 
 tables = ['customers','geolocation','order_items','order_payments','order_reviews','orders','product_category_name_translation','products','sellers'] 
 
 
 
-conn = Connect_MySQL(username='root', password='vighnesh',host='localhost',db='ecommerce')
-print("Connection SUccessfull")
-dataframes = conn.fetch_tables_to_dfs(table_names=tables)
+try:
+    username = connectivity_info['username']
+    password = connectivity_info['password']
+    host = connectivity_info['host']
+    db = connectivity_info['db']    
 
-print("Data retrive successful.")
+    # create a object of connection to mysql server.
+    conn = Connect_MySQL(username=username, password=password,host=host,db=db)
+    print("Connection SUccessfull")
 
 
-for table in tables:
-    dataframes[table].to_csv(f"./data/raw/{table}.csv")
-    print(f'{table} saved successfully.')
+   
+except Exception as e:
+    print("Failed to connect to server.")
+    print(e)
 
+
+
+
+try: 
+    path =  os.path.join('data','raw')
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    dataframes = conn.fetch_tables_to_dfs(table_names=tables)
+    print("Data retrive successful.")
+    
+
+    for table in tables:
+        
+        dataframes[table].to_csv(f"./data/raw/{table}.csv")
+        print(f'{table} saved successfully.')
+
+
+
+except OSError as e:
+    print("Check the save path exist or not.")
+
+except Exception as e :
+    print(f"Failed to load data. {e}")
